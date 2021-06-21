0e08ad7878013fbe4dd6e564f347dfc53a2fe39b
r0naldy <82387346+r0naldy@users.noreply.github.com>
2021-06-21 13:14:56 -0500

    Add files via upload

---
+++ EXAMENRSIL/examenrsil/src/main/java/pe/edu/upeu/ril/examen/App.java
@@ -0,0 +1,25 @@
+package pe.edu.upeu.ril.examen;
+public class App {
+
+    public static void main(String[] args){
+
+        
+
+
+        //recursividad
+
+        ResolucionExamen HM= new ResolucionExamen();
+
+        System.out.println(HM.potencia(2,5));
+
+
+    
+
+    }
+
+    
+
+
+}
+
+
---
+++ EXAMENRSIL/examenrsil/src/main/java/pe/edu/upeu/ril/examen/lt.java
@@ -0,0 +1,9 @@
+package pe.edu.upeu.ril.examen;
+
+public class lt {
+
+    public static int leer(int i, String string) {
+        return 0;
+    }
+
+}
